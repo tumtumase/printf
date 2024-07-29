34f6b9f86152f0eafb40d28020cb3f835b7d1285
tumtumase <moecabolerato@gmail.com>
2024-07-29 12:23:25 -0700

    I am not going anywhere printf 0 task

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void)
+{
+    _printf("Hello, %s!\n", "world");
+    _printf("Printing a character: %c\n", 'A');
+    _printf("Printing a percent sign: %%\n");
+    return (0);
+}
